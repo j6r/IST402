b2f6ee686347c3d2fc24a610c1e5a666ff03cbc5
j6r <32883148+j6r@users.noreply.github.com>
2017-04-10 01:37:08 -0700

    Dump trip and station headings

--- util/get_headings.py
+++ util/get_headings.py
@@ -15,7 +15,6 @@ data_dir = os.path.join(cfg['ingestion_settings']['data_directory'])
 # for each system in the config file
 for source in cfg['datasources'].keys():
     trip_dir = os.path.join(data_dir, source, cfg['ingestion_settings']['trips_directory'])
-    mappings = cfg['datasources'][source]['trip_fields']
 
     # if the system's trip directory is present
     if os.path.isdir(trip_dir):
@@ -24,7 +23,24 @@ for source in cfg['datasources'].keys():
         for file in os.listdir(trip_dir):
 
             if file.endswith('.csv'):
-                with open(os.path.join(trip_dir, file), 'r') as ifh, open('/Users/redjen/Desktop/headings.csv', 'a') as ofh:
+                with open(os.path.join(trip_dir, file), 'r') as ifh, open('/Users/redjen/Desktop/trip_headings.csv', 'a') as ofh:
+                    line = ifh.readline()
+                    write_headings = writer(ofh)
+                    headings = [source] + [file] + line.split(sep=',')
+                    write_headings.writerow(headings)
+
+
+for source in cfg['datasources'].keys():
+    stations_dir = os.path.join(data_dir, source, 'stations')
+
+    # if the system's station directory is present
+    if os.path.isdir(stations_dir):
+
+        # for every file in the station directory
+        for file in os.listdir(stations_dir):
+
+            if file.endswith('.csv'):
+                with open(os.path.join(stations_dir, file), 'r') as ifh, open('/Users/redjen/Desktop/station_headings.csv', 'a') as ofh:
                     line = ifh.readline()
                     write_headings = writer(ofh)
                     headings = [source] + [file] + line.split(sep=',')
