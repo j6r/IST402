6445fa0e2c6904b1606ba1e3ef7f2827ab5e9618
j6r <32883148+j6r@users.noreply.github.com>
2017-04-18 12:44:33 -0700

    Fixing elevation get station queries to handle more bad data

--- ingestion/add_elevation.py
+++ ingestion/add_elevation.py
@@ -8,8 +8,8 @@ API_KEY = settings.get_config()['google_api']['api_key']
 GET_START_STATIONS_QUERY = """
 SELECT start_station_id, start_station_latitude, start_station_longitude
     FROM start_station
-    WHERE start_station_latitude IS NOT NULL AND start_station_longitude IS NOT null
-        AND start_station_latitude IS NOT '' AND start_station_longitude IS NOT ''
+    WHERE start_station_latitude NOT IN('#N/A', '')
+        AND start_station_longitude  NOT IN ('#N/A', '')
         AND start_station_elevation IS NULL
     LIMIT 500
 """
@@ -17,8 +17,8 @@ SELECT start_station_id, start_station_latitude, start_station_longitude
 GET_END_STATIONS_QUERY = """
 SELECT end_station_id, end_station_latitude, end_station_longitude
     FROM end_station
-    WHERE end_station_latitude IS NOT NULL AND end_station_longitude IS NOT null
-        AND end_station_latitude IS NOT '' AND end_station_longitude IS NOT ''
+    WHERE end_station_latitude NOT IN ('#N/A', '') 
+        AND end_station_longitude NOT IN ('#N/A', '')
         AND end_station_elevation IS NULL
     LIMIT 500
 """
@@ -52,19 +52,19 @@ def main():
 
     db_conn = util.get_database_connection()
 
-    # while True:
-    #     stations = db_conn.execute(GET_START_STATIONS_QUERY).fetchall()
-    #     locations = list(map(lambda x: (x[1], x[2]), stations))
-    #     if len(locations) > 0:
-    #         location_results = elevation(gmaps, locations)
-    #         updates = []
-    #         for l, s in zip(location_results, stations):
-    #             updates.append((l['elevation'], s[1], s[2]))
-    #
-    #         db_conn.executemany(UPDATE_START_STATION_QUERY, updates)
-    #         db_conn.commit()
-    #     else:
-    #         break
+    while True:
+        stations = db_conn.execute(GET_START_STATIONS_QUERY).fetchall()
+        locations = list(map(lambda x: (x[1], x[2]), stations))
+        if len(locations) > 0:
+            location_results = elevation(gmaps, locations)
+            updates = []
+            for l, s in zip(location_results, stations):
+                updates.append((l['elevation'], s[1], s[2]))
+
+            db_conn.executemany(UPDATE_START_STATION_QUERY, updates)
+            db_conn.commit()
+        else:
+            break
 
     while True:
         stations = db_conn.execute(GET_END_STATIONS_QUERY).fetchall()
