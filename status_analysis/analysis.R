1e8211a58d42bedee2c5a07029c5ca2603297ee4
j6r <32883148+j6r@users.noreply.github.com>
2017-04-23 17:47:05 -0700

    Basic bike availability with ggplot

---
+++ status_analysis/analysis.R
@@ -0,0 +1,55 @@
+library(padr)
+library(ggplot2)
+library(dplyr)
+library(forecast)
+
+
+get_data <- function() {
+  # Gets the station data
+  #
+  # Returns:
+  #   The station data as a dataframe
+  
+  df <- read.csv("../data/station_197_extract.csv")
+  df$timestamp<-as.POSIXct(df$timestamp,format="%Y-%m-%d %H:%M")
+  return(df)
+}
+
+reduce_to_hours <- function(df) {
+  # Aggregates by hour and fills in missing values with the previous.
+  #
+  # Args:
+  #   df: dataframe containing station data
+  #
+  # Returns:
+  #   Dataframe with timestamp and min bikes available
+  
+  df1 <- df %>% 
+    thicken('hour', by=timestamp()) %>% 
+    group_by(timestamp_hour) %>%
+    summarise(hour_amount = min(bikes_available)) %>% 
+    pad() %>% 
+    fill_by_value(hour_amount)
+  names(df1) <- c("time", "bikes")
+  return(df1)
+  
+  #  ggplot(df_bikes, aes(time, bikes)) + geom_point() +  geom_smooth() +
+  #    labs(x="Time", y="Bikes Available", title="Bike Availability July/August")
+}
+
+
+
+
+df <- get_data()
+df_bikes <- reduce_to_hours(df)
+tser <- ts(df_bikes$bikes, frequency = 24, start = c(2015, 9))
+df_forecast <- HoltWinters(tser)
+# plot(df_forecast)
+
+# Predicted bikes available
+df_pred <- predict(df_forecast, n.ahead = 24, level = .85)
+
+predicted <- as.data.frame(df_pred)
+names(predicted) <- c("bikes")
+ggplot(data = predicted, aes(x = as.numeric(row.names(predicted)), y = predicted$bikes)) + 
+  geom_line() + ggtitle("Bike/dock availability 9/1/2016") + xlab("Hour") + ylab("Bikes/Docks")
